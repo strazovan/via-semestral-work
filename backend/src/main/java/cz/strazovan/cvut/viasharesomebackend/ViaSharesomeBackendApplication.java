a1b72639e32b844969edacbfe8d2b445f91a4ffc
David Stražovan <strazdav@fel.cvut.cz>
2021-11-17 16:06:18 +0100

    backend project with codegen

---
+++ backend/src/main/java/cz/strazovan/cvut/viasharesomebackend/ViaSharesomeBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.strazovan.cvut.viasharesomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaSharesomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaSharesomeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cz/strazovan/cvut/viasharesomebackend/ViaSharesomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.strazovan.cvut.viasharesomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaSharesomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
