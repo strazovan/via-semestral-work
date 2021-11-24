d8a1783e28e08e26b636d7b26c0dd58fe380549b
Stražovan David <strazovan.d@gmail.com>
2021-11-24 14:03:41 +0100

    adds FilesController

--- backend/src/main/java/cz/strazovan/cvut/viasharesomebackend/controllers/FilesController.java
+++ backend/src/main/java/cz/strazovan/cvut/viasharesomebackend/controllers/FilesController.java
@@ -1,2 +1,15 @@
-package cz.strazovan.cvut.viasharesomebackend.controllers;public class FilesController {
+package cz.strazovan.cvut.viasharesomebackend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import cz.strazovan.cvut.viasharesomebackend.api.controllers.*;
+import cz.strazovan.cvut.viasharesomebackend.api.model.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Component
+public class FilesController implements V1ApiDelegate {
+
 }
