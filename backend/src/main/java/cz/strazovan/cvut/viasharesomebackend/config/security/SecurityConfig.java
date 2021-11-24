891d72f1a84a27e912dcb578c5ab1458737d171c
Stražovan David <strazovan.d@gmail.com>
2021-11-24 13:40:17 +0100

    google oauth

---
+++ backend/src/main/java/cz/strazovan/cvut/viasharesomebackend/config/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package cz.strazovan.cvut.viasharesomebackend.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .logout()
+                .logoutSuccessUrl(this.frontendUrl)
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl(this.frontendUrl);
+    }
+
+}
