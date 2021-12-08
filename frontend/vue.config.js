d5954d13e0b3a43093f8f924d0e97770fd808b43
David Stražovan <strazdav@fel.cvut.cz>
2021-12-08 20:44:32 +0100

    adds axios and set up vue proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/be': {
+        target: 'http://localhost:1808/'
+      }
+    }
+  }
 }
